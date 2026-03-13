5030960bfd0ad798985c4e4686f330e22842d5c6
UdayGoshike <udaygoshike2@gmail.com>
2026-03-13 15:14:41 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (13.93%) - LeetSync

--- 443-string-compression/string-compression.cpp
+++ 443-string-compression/string-compression.cpp
@@ -5,33 +5,38 @@ public:
             return 1;
         int c = 1;
         int l = 1;
+        int in=0;
 
-        vector<char> a;
+        
         while (l < chars.size()) {
             if (chars[l] == chars[l - 1]) {
                 c++;
-
-            } else{
-                a.push_back(chars[l - 1]);
+            } else {
+                chars[in++]=chars[l-1];
                 if (c > 1) {
                     string s = to_string(c);
                     for (int i = 0; i < s.size(); i++) {
-                        a.push_back(s[i]);
+                        chars[in++]=s[i];
                     }
                 }
                 c = 1;
-            } 
-             
+            
+            }
+
             l++;
         }
-        a.push_back(chars[l-1]);
+        
+         chars[in++]=chars[l-1];
+        
         if (c > 1) {
-                    string s = to_string(c);
-                    for (int i = 0; i < s.size(); i++) {
-                        a.push_back(s[i]);
-                    }
-                }
-        chars = a;
+            string s = to_string(c);
+            for (int i = 0; i < s.size(); i++) {
+                chars[in++]=s[i];
+            }
+            
+        }
+        chars.erase(chars.begin()+in,chars.end());
+       
 
         return chars.size();
     }

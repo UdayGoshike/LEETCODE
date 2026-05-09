4ebc689bb594d225fa6c77b0ab6008b1bff3f36f
Goshike Uday Kumar <udaygoshike2@gmail.com>
2026-05-09 10:32:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (17.77%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
 int fi(int n,vector<int>&dp){
-    if(n<=1){
-        return n;
-    }
     if(dp[n]!=-1){
         return dp[n];
     }
+    if(n<=1){
+        return n;
+    }
+   
     return dp[n]=fi(n-1,dp)+fi(n-2,dp);
 }
     int fib(int n) {

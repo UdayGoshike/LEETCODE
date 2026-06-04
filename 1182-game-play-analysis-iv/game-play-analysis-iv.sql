ad47966d535a09f19fcf2349770b3e0aba208827
Goshike Uday Kumar <udaygoshike2@gmail.com>
2026-06-04 13:48:42 +0530

    Time: 4136 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,2 @@
+    # Write your MySQL query statement below
+    SELECT ROUND(COUNT(DISTINCT a.player_id )/(SELECT COUNT( DISTINCT player_id) FROM Activity),2) AS fraction FROM Activity a JOIN (SELECT player_id,MIN(event_date) AS fl FROM Activity GROUP BY player_id)f ON a.player_id=f.player_id AND DATEDIFF(a.event_date,f.fl)=1;

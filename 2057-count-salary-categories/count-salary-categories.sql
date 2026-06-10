d4e5dfd83d3efc21e2c3468939df59d79bd1cf0b
Goshike Uday Kumar <udaygoshike2@gmail.com>
2026-06-10 19:48:29 +0530

    Time: 1623 ms (30.72%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+-- SELECT CASE WHEN income<20000 THEN 'Low Salary' WHEN income<=50000 THEN 'Average Salary' ELSE 'High Salary' END as category,count(*) as accounts_count FROM Accounts group by category;
+-- THIS CANNOT SHOW CATEGORY WITH ZERO;
+SELECT 'Low Salary' as category,count(*) as accounts_count FROM Accounts WHERE income<20000
+union all
+SELECT 'Average Salary' as category,count(*) as accounts_count FROM Accounts WHERE income BETWEEN 20000 AND 50000
+union all
+SELECT 'High Salary' as category,count(*) as accounts_count FROM Accounts WHERE income>50000;       
+

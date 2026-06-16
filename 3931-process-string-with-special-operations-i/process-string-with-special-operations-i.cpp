a8fe529cdcfb614145df1fa4460aa24b21a25fec
Goshike Uday Kumar <udaygoshike2@gmail.com>
2026-06-16 14:51:36 +0530

    Time: 5 ms (45.83%) | Memory: 39.7 MB (87.50%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string r="";
+        for(char c:s){
+            if(c=='*'){
+                if(!r.empty()){
+                    r.pop_back();
+                }
+            }else if (c=='#'){
+                r+=r;
+            }else if (c=='%'){
+                reverse(r.begin(),r.end());
+            }else{
+                r+=c;
+            }
+        }
+        return r;
+    }
+};

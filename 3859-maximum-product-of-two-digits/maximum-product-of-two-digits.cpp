80e5dba0766228c286369274d12aa396f7836896
Goshike Uday Kumar <udaygoshike2@gmail.com>
2026-07-26 01:53:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (10.39%) - LeetSync

---
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+    string s=to_string(n);
+    if(s.size()==1){
+        return n;
+    }        
+
+sort(s.begin(),s.end());
+        return (s[s.size()-1]-'0') *(s[s.size()-2]-'0');
+    }
+};

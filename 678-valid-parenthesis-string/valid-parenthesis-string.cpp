1417fc04f98ea8431b826da74ce16aeefad55899
Goshike Uday Kumar <udaygoshike2@gmail.com>
2026-08-08 20:39:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (27.89%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    // RECURSIVE WAY O(3^N)
+
+    // bool h(string s,int in,int c){
+    //     if(c<0){
+    //         return false;
+    //     }
+    //     if(in ==s.size()){
+    //         return c==0;
+    //     }
+    //     if(s[in]=='('){
+    //          return h(s,in+1,c+1);
+    //     }
+    //     if(s[in]==')'){
+    //        return h(s,in+1,c-1);
+    //     }
+    //     return h(s,in+1,c+1)||h(s,in+1,c-1)||h(s,in+1,c);
+    // }
+    bool checkValidString(string s) {
+        // return h(s,0,0);
+        int min = 0;
+        int max = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                min++;
+                max++;
+            } else if (s[i] == ')') {
+                if (min == 0 && max == 0) {
+                    return false;
+                } 
+                if (min > 0) {
+                    min--;
+                } 
+                if (max > 0) {
+                    max--;
+                }
+            } else {
+                if (min > 0) {
+                    min--;
+                    max++;
+                } else {
+                    max++;
+                }
+            }
+        }
+        return min == 0;
+    }
+};

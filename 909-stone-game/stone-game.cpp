5cbb43f62548029560f8283439b2a52608f590a5
Goshike Uday Kumar <udaygoshike2@gmail.com>
2026-08-09 09:22:15 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (66.12%) - LeetSync

---
+++ 909-stone-game/stone-game.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        int l = 0;
+        int r = piles.size() - 1;
+        int a = 0;
+        int b = 0;
+        bool f = true;
+        while (l <= r) {
+            if (f == true) {
+                if (piles[l] >= piles[r]) {
+                    a += piles[l];
+                    l++;
+                } else {
+                    a += piles[r];
+                    r--;
+                }
+                f=false;
+            } else if (f == false) {
+                if (piles[l] >= piles[r]) {
+                    a += piles[l];
+                    l++;
+                } else {
+                    a += piles[r];
+                    r--;
+                }
+                f=true;
+            }
+        }
+        return a>b;
+    }
+};
